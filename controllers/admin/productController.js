cd10a39fa2c11bdc62085f0b083b51827b08a23f
ashnasherinp <sherinashna2@gmail.com>
2024-12-24 16:52:58 +0400

    block/unblock products

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -155,7 +155,26 @@ const removeProductOffer = async(req,res)=>{
     }
 }
 
+const blockProduct = async(req,res)=>{
+    try {
+        let id =req.query.id
+        await Product.updateOne({_id:id},{$set:{isBlocked:true}})
+        res.redirect("/admin/products")
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
 
+
+const unblockProduct =async(req,res)=>{
+    try {
+        let id =req.query.id
+        await Product.updateOne({_id:id},{$set:{isBlocked:false}})
+        res.redirect("/admin/products")
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
 
 
 
@@ -165,4 +184,7 @@ module.exports ={
     getAllProducts,
     addProductOffer,
     removeProductOffer,
+    blockProduct,
+    unblockProduct,
+
 }
